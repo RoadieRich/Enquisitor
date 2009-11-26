d3c2294ee4db5bb3371bb4d69dc4f52f5aa20c89
Rich Lovely <rich@roadierich.co.uk>
2009-11-26 05:16:36 +0000

    Yay, code\!

--- util.py
+++ util.py
@@ -6,3 +6,9 @@ def roll(n, s):
     if n==1:
         return random.randint(1,s)
     return sum(random.randint(1,s) for x in xrange(n))
+
+def null(object):
+    def __cmp__(self, other):
+        return False
+    def __bool__(self):
+        return False
